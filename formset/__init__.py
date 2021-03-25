aaad8da288430251d4ee4f0cab88160737bbbe0f
Takahiro Ueda <tueda@st.seikei.ac.jp>
2021-03-25 14:34:16 +0900

    refactor: change the packaging structure (#3)
    Put the module into the "formset" directory. "py.typed" is also added
    (see PEP 561).
    
    NOTE: formset/formset.py can still be run as a standalone script.

---
+++ formset/__init__.py
@@ -0,0 +1,6 @@
+"""Determine suitable FORM setup parameters."""
+
+from .formset import Setup, SystemInfo
+from .formset import main as main  # noqa: F401
+
+__all__ = ["Setup", "SystemInfo"]
---
+++ formset/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point."""
+
+from .formset import main
+
+if __name__ == "__main__":
+    main()
